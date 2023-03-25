8f692df3e63c04bc09f2f008ddd2bfb0538ba92a
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-03-25 15:03:51 +0100

    add sse example

--- index.ts
+++ index.ts
@@ -1,18 +1,56 @@
 import zmq from "zeromq";
+import express from "express";
+import fs from "fs";
 
 const sock = zmq.socket("sub");
 
-// TODO:  express (or Nextjs) with SSE, frontend with RxJs observable
+const TCP_ENDPOINT = "tcp://pubsub.besteffort.ndovloket.nl:7658";
+const TCP_ENVELOPE = "/EBS/KV6posinfo";
+const TCP_PORT = 7658;
 
-sock.connect("tcp://pubsub.besteffort.ndovloket.nl:7658");
-sock.subscribe("/EBS/KV6posinfo");
-console.log("Subscriber connected to port 7658");
+async function run() {
+  sock.connect(TCP_ENDPOINT);
+  sock.subscribe(TCP_ENVELOPE);
+  console.log(`Subscriber connected to port ${TCP_PORT}`);
 
-sock.on("message", function (topic, message) {
-  console.log(
-    "received a message related to:",
-    topic,
-    "containing message:",
-    message
-  );
-});
+  sock.on("message", function (topic, message) {
+    console.log(
+      "received a message related to:",
+      topic.toString("utf-8"),
+      "containing message:",
+      message
+    );
+  });
+
+  const app = express();
+
+  app.get("/events", async function (req, res) {
+    console.log("Got /events");
+    res.set({
+      "Cache-Control": "no-cache",
+      "Content-Type": "text/event-stream",
+      Connection: "keep-alive",
+    });
+    res.flushHeaders();
+
+    // Tell the client to retry every 10 seconds if connectivity is lost
+    res.write("retry: 10000\n\n");
+    let count = 0;
+
+    while (true) {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      console.log("Emit", ++count);
+      // Emit an SSE that contains the current 'count' as a string
+      res.write(`data: ${count}\n\n`);
+    }
+  });
+
+  const index = fs.readFileSync("./index.html", "utf8");
+  app.get("/", (req, res) => res.send(index));
+
+  app.listen(3000);
+  console.log("Listening on port 3000");
+}
+
+run().catch((err) => console.log(err));
