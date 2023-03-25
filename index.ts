c79f08664e1752636abfdd4f568c0ff1e4664c2e
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-03-25 15:14:07 +0100

    emit on socket msg

--- index.ts
+++ index.ts
@@ -9,19 +9,6 @@ const TCP_ENVELOPE = "/EBS/KV6posinfo";
 const TCP_PORT = 7658;
 
 async function run() {
-  sock.connect(TCP_ENDPOINT);
-  sock.subscribe(TCP_ENVELOPE);
-  console.log(`Subscriber connected to port ${TCP_PORT}`);
-
-  sock.on("message", function (topic, message) {
-    console.log(
-      "received a message related to:",
-      topic.toString("utf-8"),
-      "containing message:",
-      message
-    );
-  });
-
   const app = express();
 
   app.get("/events", async function (req, res) {
@@ -37,13 +24,30 @@ async function run() {
     res.write("retry: 10000\n\n");
     let count = 0;
 
-    while (true) {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+    // while (true) {
+    //   await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      console.log("Emit", ++count);
-      // Emit an SSE that contains the current 'count' as a string
+    //   console.log("Emit", ++count);
+    //   // Emit an SSE that contains the current 'count' as a string
+    //   res.write(`data: ${count}\n\n`);
+    // }
+
+    sock.connect(TCP_ENDPOINT);
+    sock.subscribe(TCP_ENVELOPE);
+    console.log(`Subscriber connected to port ${TCP_PORT}`);
+
+    sock.on("message", function (topic, message) {
+      console.log(
+        "Emit: received a message related to:",
+        topic.toString("utf-8"),
+        "containing message:",
+        message
+      );
+
+      count++;
+
       res.write(`data: ${count}\n\n`);
-    }
+    });
   });
 
   const index = fs.readFileSync("./index.html", "utf8");
